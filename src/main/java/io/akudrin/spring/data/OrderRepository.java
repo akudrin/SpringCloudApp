cc246d2c5215cd6b738ff88d15d351ba691a55f1
Andrei Kudrin <andreikudrin@gmail.com>
2019-04-15 09:15:06 +0100

    Enabling configuration properties in OrderController

--- src/main/java/io/akudrin/spring/data/OrderRepository.java
+++ src/main/java/io/akudrin/spring/data/OrderRepository.java
@@ -1,9 +1,14 @@
 package io.akudrin.spring.data;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import io.akudrin.spring.Order;
+import io.akudrin.spring.User;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
+	List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 
 }
--- src/main/java/io/akudrin/spring/web/OrderController.java
+++ src/main/java/io/akudrin/spring/web/OrderController.java
@@ -1,8 +1,13 @@
 package io.akudrin.spring.web;
+
 import javax.validation.Valid;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,52 +23,64 @@ import io.akudrin.spring.data.OrderRepository;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties(prefix="taco.orders")
 public class OrderController {
-  
-  private OrderRepository orderRepo;
 
-  public OrderController(OrderRepository orderRepo) {
-    this.orderRepo = orderRepo;
-  }
-  
-  @GetMapping("/current")
-  public String orderForm(@AuthenticationPrincipal User user, 
-      @ModelAttribute Order order) {
-    if (order.getDeliveryName() == null) {
-      order.setDeliveryName(user.getFullname());
-    }
-    if (order.getDeliveryStreet() == null) {
-      order.setDeliveryStreet(user.getStreet());
-    }
-    if (order.getDeliveryCity() == null) {
-      order.setDeliveryCity(user.getCity());
-    }
-    if (order.getDeliveryState() == null) {
-      order.setDeliveryState(user.getState());
-    }
-    if (order.getDeliveryZip() == null) {
-      order.setDeliveryZip(user.getZip());
-    }
-    
-    return "orderForm";
-  }
+	private OrderRepository orderRepo;
 
-  // tag::processOrderWithAuthenticationPrincipal[]
-  @PostMapping
-  public String processOrder(@Valid Order order, Errors errors, 
-      SessionStatus sessionStatus, 
-      @AuthenticationPrincipal User user) {
-    
-    if (errors.hasErrors()) {
-      return "orderForm";
-    }
-    
-    order.setUser(user);
-    
-    orderRepo.save(order);
-    sessionStatus.setComplete();
-    
-    return "redirect:/";
-  }
+	public OrderController(OrderRepository orderRepo) {
+		this.orderRepo = orderRepo;
+	}
+
+	private int pageSize = 20;
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	@GetMapping("/current")
+	public String orderForm(@AuthenticationPrincipal User user, @ModelAttribute Order order) {
+		if (order.getDeliveryName() == null) {
+			order.setDeliveryName(user.getFullname());
+		}
+		if (order.getDeliveryStreet() == null) {
+			order.setDeliveryStreet(user.getStreet());
+		}
+		if (order.getDeliveryCity() == null) {
+			order.setDeliveryCity(user.getCity());
+		}
+		if (order.getDeliveryState() == null) {
+			order.setDeliveryState(user.getState());
+		}
+		if (order.getDeliveryZip() == null) {
+			order.setDeliveryZip(user.getZip());
+		}
+
+		return "orderForm";
+	}
+
+	// tag::processOrderWithAuthenticationPrincipal[]
+	@PostMapping
+	public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus,
+			@AuthenticationPrincipal User user) {
+
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
+
+		order.setUser(user);
+
+		orderRepo.save(order);
+		sessionStatus.setComplete();
+
+		return "redirect:/";
+	}
+
+	@GetMapping
+	public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+		Pageable pageable = PageRequest.of(0, pageSize);
+		model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+		return "orderList";
+	}
 
 }
